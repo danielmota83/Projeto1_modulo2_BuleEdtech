b8b4645646d206b9edbaaa2252424ee11250c92c
Daniel Mota <101141570+danielmota83@users.noreply.github.com>
2022-05-22 10:43:17 -0300

    alterações responsividade

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 
-let elementoMudaTexto = document.querySelector(".texto2");
-let elementoImg = document.querySelector(".imagem");
-let elementoBtn = document.querySelector("button");
+let mudaTexto = document.querySelector(".texto2");
+let mudaImg = document.querySelector(".imagem");
+let mudaBtn = document.querySelector("button");
 
-elementoBtn.addEventListener("click", () => {
-  if (elementoBtn.value == "primeiro") {
-    elementoImg.src = "./assets/imagens/mostrando1.png";
-    elementoMudaTexto.innerText = "Marcha Lenta mostrando a cara, clique novamente e se surpreenda!";
-    elementoBtn.value = "segundo";
-} else if (elementoBtn.value == "segundo") {
-    elementoImg.src = "./assets/imagens/mostrando2.png";
-    elementoMudaTexto.innerText = "Não adiantou muita coisa né, mas clique novamente que digo quem é.";
-    elementoBtn.value = "terceiro";
-} else if (elementoBtn.value == "terceiro") {
-    elementoImg.src = "./assets/imagens/mostrando3.png";
-    elementoMudaTexto.innerText = "Com estudo, todos podemos ser programadores, clique e confirme.";
-    elementoBtn.value = "quarto";
-  } else if (elementoBtn.value == "quarto") {
-    elementoImg.src = "./assets/imagens/mostrando4.png";
-    elementoMudaTexto.innerText = "Marcha Lenta é o Daniel Mota, a pessoa que programou esta página!";
-    elementoBtn.value = "quinto";
+
+mudaBtn.addEventListener("click", () => {
+  if (mudaBtn.value == "primeiro") {
+    mudaImg.src = "./assets/imagens/mostrando1.png";
+    mudaTexto.innerText = "Marcha Lenta mostrando a cara, clique novamente e se surpreenda!";
+    mudaBtn.value = "segundo";
+} else if (mudaBtn.value == "segundo") {
+    mudaImg.src = "./assets/imagens/mostrando2.png";
+    mudaTexto.innerText = "Não adiantou muita coisa né, mas clique novamente que digo quem é.";
+    mudaBtn.value = "terceiro";
+} else if (mudaBtn.value == "terceiro") {
+    mudaImg.src = "./assets/imagens/mostrando3.png";
+    mudaTexto.innerText = "Com estudo, todos podemos ser programadores, clique e confirme.";
+    mudaBtn.value = "quarto";
+  } else if (mudaBtn.value == "quarto") {
+    mudaImg.src = "./assets/imagens/mostrando4.png";
+    mudaTexto.innerText = "Marcha Lenta é o Daniel Mota, a pessoa que programou esta página!";
+    mudaBtn.value = "quinto";
+    mudaBtn.innerText = "Reset Jogo"
   } else {
-    elementoImg.src = "./assets/imagens/escondido.png";
-    elementoMudaTexto.innerText = "Marcha Lenta está de cabeça baixa, clique no botão abaixo e veja quem é.";
-    elementoBtn.value = "primeiro";
+    mudaImg.src = "./assets/imagens/escondido.png";
+    mudaTexto.innerText = "Marcha Lenta está de cabeça baixa, clique no botão abaixo e veja quem é.";
+    mudaBtn.value = "primeiro";
+    mudaBtn.innerText = "Clique e descubra"
   }
 });
